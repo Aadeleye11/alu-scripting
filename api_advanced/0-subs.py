9855fc34b296fd6a6fb9b566facac789eb3f25c0
Ayomide <a.adeleye@alustudent.com>
2025-07-14 20:44:00 +0000

    m

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
 """
-This module contains a function to query Reddit API
-and return number of subscribers for a subreddit.
+This module defines a function that returns the number of subscribers for a subreddit.
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns number of subscribers for a given subreddit"""
+    """
+    Returns the number of subscribers for a given subreddit.
+    If subreddit is invalid or request fails, return 0.
+    """
     client_id = "BLDc0NYEGo_wuYewe9CZdw"
     client_secret = "k_A_JrFDOnivsGdAxg0ai6BbefBkRw"
-    user_agent = (
-        "python:RedditAPIProject:v1.0.0 "
-        "(by /u/FlowPsychological419)"
-    )
+    user_agent = "python:RedditAPIProject:v1.0.0 (by /u/FlowPsychological419)"
 
-    # Get access token
     auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
     data = {"grant_type": "client_credentials"}
     headers = {"User-Agent": user_agent}
 
     token_response = requests.post(
         "https://www.reddit.com/api/v1/access_token",
-        auth=auth,
-        data=data,
-        headers=headers
+        auth=auth, data=data, headers=headers
     )
 
     if token_response.status_code != 200:
@@ -34,11 +29,10 @@ def number_of_subscribers(subreddit):
     token = token_response.json().get("access_token")
     headers["Authorization"] = "bearer {}".format(token)
 
-    # Request subreddit info
     url = "https://oauth.reddit.com/r/{}/about".format(subreddit)
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         return response.json().get("data", {}).get("subscribers", 0)
-
-    return 0
+    else:
+        return 0
